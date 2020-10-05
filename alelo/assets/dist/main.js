7df57d77a43da7f117026e690e38468db990ab8f
Dung Huynh <dung.huynh@readytech.io>
2020-10-05 14:08:07 +0700

    edit common.js

--- alelo/assets/dist/main.js
+++ alelo/assets/dist/main.js
@@ -123,6 +123,12 @@ var CommonJs = function () {
           currentModal.closest("div[id^='addclass']").nextAll("div[id^='addclass']").first().modal('show');
         });
       });
+
+      $('.table-lst-student input[type=checkbox]').on('change', function () {
+        if ($(this).prop('checked') == true) $(".btn-show").show(); // checked
+
+        else if (!$(".table-lst-student input[type=checkbox]:checked").length) $(".btn-show").hide(); // unchecked
+      });
     }
   };
 }();
