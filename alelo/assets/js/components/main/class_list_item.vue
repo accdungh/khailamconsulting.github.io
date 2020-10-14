beb1dbb86c973062d782a42a18f095889bada954
Dung Huynh <dung.huynh@readytech.io>
2020-10-14 14:18:03 +0700

    Class List - Class links Cursor

--- alelo/assets/js/components/main/class_list_item.vue
+++ alelo/assets/js/components/main/class_list_item.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="form-group">
-    <h2
-      class="class-title"
-      v-text="classData.name"
-      @click="select(classData.id)"
-    ></h2>
+    <h2 class="class-title">
+      <a href="javascript:void(0)" v-text="classData.name"
+      @click="select(classData.id)"></a>
+    </h2>
     <div class="row">
       <div class="col-md-12 mb-2 mt-2 f-m-20">Summary</div>
       <div class="col-md-12 mb-2 mt-2 f-n-16 text-right">
