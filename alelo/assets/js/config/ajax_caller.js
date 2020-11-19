87ef3cc497ad87f542e24fb7793184f9275e34ef
pmquan <pmquan@gmail.com>
2020-11-18 16:25:10 -0800

    Update dummy API

--- alelo/assets/js/config/ajax_caller.js
+++ alelo/assets/js/config/ajax_caller.js
@@ -1,53 +1,60 @@
 import axios from "axios";
 
-const API_PATH = window.location.href.replace(/(.+)\/[^\/]*#.*/, '$1');
+const API_PATH = "https://sim-dev.alelo.com/api/dummy_instructor";
 
 export default {
   classList() {
-    return axios.get(`${API_PATH}/example_data/list.json`, {
+    //return axios.get(`${API_PATH}/example_data/list.json`, {
+      return axios.get(`${API_PATH}/class/list`, {
       withCredentials: true
     });
   },
   archivedClassList() {
-    return axios.get(`${API_PATH}/example_data/archived_list.json`, {
+    return axios.get(`${API_PATH}/class/archive/list`, {
       withCredentials: true
     });
   },
-  classDetail() {
-    return axios.get(`${API_PATH}/example_data/detail.json`, {
-      withCredentials: true
+  classDetail(id) {
+  //  return axios.get(`${API_PATH}/example_data/detail.json`, {
+  return axios.get(`${API_PATH}/class/view/$`+id, {
+    withCredentials: true
     });
   },
   createClass(data) {
     // FIXME: Use method POST with real API
-    return axios.get(`${API_PATH}/example_data/detail.json`, data, {
+    return axios.post(`${API_PATH}/class/create`, data, {
       withCredentials: true
     });
   },
   updateClass(id, data) {
     // FIXME: Use method PUT with real API
-    return axios.get(`${API_PATH}/example_data/detail.json`, data, {
+    return axios.post(`${API_PATH}/class/update/`+id, data, {
       withCredentials: true
     });
   },
   deleteClass(id) {
     // FIXME: Use method DELETE with real API
-    return axios.get(`${API_PATH}/example_data/detail.json`, {
+    return axios.post(`${API_PATH}/class/delete/`+id, {
       withCredentials: true
     });
   },
   userSetting(id) {
-    return axios.get(`${API_PATH}/example_data/user.json`, {
+    return axios.get(`${API_PATH}/user/info`, {
       withCredentials: true
     });
   },
   updateUserSetting(id, data) {
-    return axios.post(`${API_PATH}/example_data/user.json`, data, {
+    return axios.post(`${API_PATH}/user/info`, data, {
       withCredentials: true
     });
   },
   studentDetail(id) {
-    return axios.get(`${API_PATH}/example_data/student_detail.json`, {
+    return axios.get(`${API_PATH}/student/`+id, {
+      withCredentials: true
+    });
+  },
+  userInvitation(data) {
+    return axios.post(`${API_PATH}/class/invite`, {
       withCredentials: true
     });
   }
