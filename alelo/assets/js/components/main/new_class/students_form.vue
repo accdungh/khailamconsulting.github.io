b22e29c41eb1423d23432fd88ad42c3329652c05
Phong Nguyen <phong.nguyen@readytech.io>
2020-12-08 20:35:23 +0700

    New class wizard: Build the students form

--- alelo/assets/js/components/main/new_class/students_form.vue
+++ alelo/assets/js/components/main/new_class/students_form.vue
@@ -1,8 +1,29 @@
 <template>
-  <div>Students form</div>
+  <div class="students-form">
+    <label class="f-m-14 blue-light">
+      {{ $t("studentsForm.infoText") }}
+    </label>
+
+    <div class="mb-4">
+      <textarea
+        v-model="formData.students"
+        class="form-control auto-size student-list"
+        rows="15"
+      />
+    </div>
+
+    <WizardButtons
+      backStep="Back"
+      @backStepClick="emit('backStepClick')"
+      nextStep="Review"
+      @nextStepClick="submit()"
+    />
+  </div>
 </template>
 
 <script>
+import WizardButtons from "./wizard_buttons.vue";
+
 export default {
   name: "StudentsForm",
   props: {
@@ -11,6 +32,23 @@ export default {
       default() {
         return {};
       },
+    },
+  },
+  components: { WizardButtons },
+  created() {
+    this.formData = this.value;
+  },
+  methods: {
+    emit(name) {
+      this.$emit(name);
+    },
+    submit() {
+      this.$validator.validateAll().then((valid) => {
+        if (valid) {
+          this.$emit("input", this.formData);
+          this.$emit("nextStepClick");
+        }
+      });
     },
   },
 };
