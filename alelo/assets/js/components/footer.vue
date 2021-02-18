3777d2d0aac8990bf3329f4f5dec667543b04d2c
Dung Huynh <dung.huynh@readytech.io>
2021-02-18 19:30:18 +0700

    issue_103: Change the footer

--- alelo/assets/js/components/footer.vue
+++ alelo/assets/js/components/footer.vue
@@ -3,22 +3,27 @@
     <div class="bg-blue">
       <div class="container">
         <div class="row">
-          <div class="col-md-10 text-center text-md-left">
+          <div class="col-md-9 text-center text-md-left f-n-16">
             {{ $t("common.footer.copyRight") }}
           </div>
-          <div class="col-md-7 text-center text-md-left">
+          <div class="col-md-6 text-center text-md-left f-n-16">
             {{ $t("common.footer.playLearnCommunicate") }}
           </div>
-          <div class="col-md-7">
-            <ul class="list-inline f-20 mb-0 mt-md-0 text-center text-md-right">
+          <div class="col-md-9">
+            <ul class="list-inline f-20 mb-0 mt-md-0 text-center text-md-right f-n-16">
               <li class="list-inline-item">
-                <a href="/contact" class="text-white">
+                <a href="/contact" class="text-white text-decoration-underline">
                   {{ $t("common.footer.contactUs") }}
                 </a>
               </li>
               <li class="list-inline-item">
-                <a href="/tos" class="text-white">
-                  {{ $t("common.footer.legal") }}
+                <a href="/privacy" class="text-white text-decoration-underline">
+                  {{ $t("common.footer.privacyPolicy") }}
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="/tos" class="text-white text-decoration-underline">
+                  {{ $t("common.footer.termsService") }}
                 </a>
               </li>
             </ul>
